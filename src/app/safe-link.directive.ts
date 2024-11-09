ed78c94b74989ff014ba34ce6375e298487ad2b7
Yash dixit <yash9369d@gmail.com>
2024-11-09 20:26:15 +0530

    Alias, and user access in different ways

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, input } from "@angular/core";
+import { Directive, ElementRef, inject, input } from "@angular/core";
 
 
 @Directive({
@@ -10,15 +10,20 @@ import { Directive, input } from "@angular/core";
 })
 
 export class SafeLinkDirective {
-    queryParam = input('myapp',{alias: 'appSafeLink'})
+    queryParam = input('myapp',{alias: 'appSafeLink'});
+    private hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
+
+
     constructor() {
 
     }
     onConfirmLeavePage(event: MouseEvent) {
         const wantsToLeave = window.confirm('Do you want to Leave');
         if (wantsToLeave) {
-            const address = (event.target as HTMLAnchorElement).href;
-            (event.target as HTMLAnchorElement).href = address + '?from=' + this.queryParam();
+            // const address = (event.target as HTMLAnchorElement).href;
+            const address = this.hostElementRef.nativeElement.href;
+            // (event.target as HTMLAnchorElement).href = address + '?from=' + this.queryParam();
+            this.hostElementRef.nativeElement.href = address + '?from=' + this.queryParam();
             return;
         }
         event?.preventDefault();
