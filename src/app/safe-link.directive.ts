70dc4fd2191eaa265f8c1ca5d26977682826c548
Yash dixit <yash9369d@gmail.com>
2024-11-09 18:46:01 +0530

    Confirm-to-leave-the-page-directive

---
+++ src/app/safe-link.directive.ts
@@ -0,0 +1,26 @@
+import { Directive, input } from "@angular/core";
+
+
+@Directive({
+    selector: 'a[appSafeLink]',
+    standalone: true,
+    host: {
+        '(click)': 'onConfirmLeavePage($event)'
+    }
+})
+
+export class SafeLinkDirective {
+    queryParam = input('myapp',{alias: 'appSafeLink'})
+    constructor() {
+
+    }
+    onConfirmLeavePage(event: MouseEvent) {
+        const wantsToLeave = window.confirm('Do you want to Leave');
+        if (wantsToLeave) {
+            const address = (event.target as HTMLAnchorElement).href;
+            (event.target as HTMLAnchorElement).href = address + '?from=' + this.queryParam();
+            return;
+        }
+        event?.preventDefault();
+    }
+}
